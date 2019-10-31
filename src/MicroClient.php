ba9dc3c3a7a9a05cb52d47c7d8c30f0acebca65f
pizepei <848733913@qq.com>
2019-10-31 23:03:44 +0800

    完善客户端

--- src/MicroClient.php
+++ src/MicroClient.php
@@ -4,6 +4,7 @@
  */
 namespace pizepei\microserviceClient;
 
+use pizepei\encryption\aes\Prpcrypt;
 
 class MicroClient
 {
@@ -12,7 +13,16 @@ class MicroClient
      * @var array
      */
     protected static $clientObj = [];
-
+    /**
+     * 配置
+     * @var array
+     */
+    private $config = [];
+    /**
+     * 缓存
+     * @var \Redis
+     */
+    private $redis = [];
     /**
      * 对应每一个app应用实例化一个对象
      * MicroClient constructor.
@@ -21,7 +31,10 @@ class MicroClient
      */
     public function __construct(\Redis $redis, array $config)
     {
-
+        # 基础配置
+        $this->config = $config;
+        # 缓存配置
+        $this->redis = $redis;
     }
     /**
      * @Author 皮泽培
@@ -35,14 +48,39 @@ class MicroClient
      */
     public static function init(\Redis $redis, array $config)
     {
-        if ($config ==[] || !isset($config['appId'])){
+        if ($config ==[] || !isset($config['appid'])){
             throw new  \Exception('config error');
         }
         # 判断对应应用的客户端是否已经实例化
-        if (static::$clientObj[$config['appId']]){
-            static::$clientObj[$config['appId']] = new static($redis,$config);
+        if (!isset(static::$clientObj[$config['appid']])){
+            static::$clientObj[$config['appid']] = new static($redis,$config);
         }
         # 返回实例化对象
-        return static::$clientObj[$config['appId']];
+        return static::$clientObj[$config['appid']];
     }
+
+    /**
+     * 请求接口
+     * @param $param
+     * @return array
+     * @throws \Exception
+     */
+    public function send($param)
+    {
+        # url
+        $url = $this->config['url'].$this->config['api'].$this->config['appid'].'.json';
+        # 确定数据
+        $Prpcrypt = new  Prpcrypt($this->config['encodingAesKey']);
+
+        $data = $Prpcrypt->yieldCiphertext(Helper()->json_encode($param),$this->config['appid'],$this->config['token']);
+        return $data;
+        $res = Helper()->httpRequest($url,Helper()->json_encode($data));
+        if ($res['code'] !== 200){throw new \Exception('httpRequest error  '.$res['code']);}
+        $body = Helper()->json_decode($res['body']);
+        if (empty($body)){throw new \Exception('body empty ');}
+        # 处理一些特殊的错误码
+        return $body;
+    }
+
+
 }
